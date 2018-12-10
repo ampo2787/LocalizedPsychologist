95ce03c128dccb9f2e332a9ba2b658b8f04ff4ff
JIHOON PARK <ampo2787@cnu.ac.kr>
2018-12-10 16:17:41 +0900

    Initial Commit

---
+++ LocalizedPsychologist/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LocalizedPsychologist
+//
+//  Created by JihoonPark on 10/12/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
